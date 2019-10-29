a772041033bc5bd874529c1d5d81e0333b654f20
Charlie Campbell <ccampbell@ukcloud.com>
2019-10-29 10:23:41 +0000

    Completed lecture 154

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import Layout from './components/Layout/Layout'
+import BurgerBuilder from './components/containers/BurgerBuilder/BurgerBuilder'
 
 function App() {
   return (
     <div>
       <Layout>
-        <p>Test</p>
+        <BurgerBuilder />
       </Layout>
     </div>
   );
